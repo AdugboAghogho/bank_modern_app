32f652196dc37264e559f9f7672cec5cd55453d6
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-30 16:03:04 +0100

    getstarted

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { discount, robot } from '../assets'
+import GetStarted from './GetStarted';
 
 const Hero = () => (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -11,12 +12,24 @@ const Hero = () => (
           alt="discount" 
           className="w-[32px] h-[32px]"
         />
-        <p className="text-white">
-          <span className='text-white'>20%</span>
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className='text-white'>20% </span>
           Discount For {" "}
-          <span className='text-white'>1 Month</span>
+          <span className='text-white'>1 Month </span>
           Account
         </p>
+      </div>
+
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+          The Next <br className='sm:block hidden' /> {" "}
+          <span className='text-gradient'>Generation</span> {" "}
+          Payment Method.
+        </h1>
+
+        <div className="ss:flex hidden md:mr-4 mr-0">
+          <GetStarted />
+        </div>
       </div>
     </div>
   </section>
