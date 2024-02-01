b353738d75bb44191eaac8cb8db0072badaf6b97
adugbo aghogho <adugboaghogho@gmail.com>
2024-02-01 11:50:19 +0100

    done with it

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { logo } from '../assets'
-import { footerLinks } from '../constants'
+import { footerLinks, socialMedia } from '../constants'
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
@@ -40,12 +40,21 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className="">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright-@ 2024 HooBank. All Rights Reserved.
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+      <p className={`${styles.paragraph} text-center text-[18px] leading-[27px]`}>
+        Copyright - @ 2024 HooBank. All Rights Reserved.
       </p>
 
-      .flex.flex-row
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img 
+            key={social.id}
+            src={social.icon} 
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`}
+          />
+        ))}
+      </div>
     </div>
   </section>
 )
--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -6,13 +6,13 @@ import GetStarted from './GetStarted';
 const Hero = () => (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 cursor-pointer">
         <img 
           src={discount} 
           alt="discount" 
           className="w-[32px] h-[32px]"
         />
-        <p className={`${styles.paragraph} ml-2`}>
+        <p className={`${styles.paragraph} ml-2  hover:text-secondary`}>
           <span className='text-white'>20% </span>
           Discount For {" "}
           <span className='text-white'>1 Month </span>
